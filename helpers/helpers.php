88ab2eafa6cde22555011532c986075a69c79216
nguyenanhung <info@nguyenanhung.com>
2023-02-13 23:33:54 +0700

    Add function _simpleViewPagination_

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,1 +1,10 @@
 <?php
+if (!function_exists('_simpleViewPagination_')) {
+    function _simpleViewPagination_($data = array())
+    {
+        $page = new \nguyenanhung\Libraries\Pagination\Pagination\SimplePagination();
+        $page->setData($data);
+
+        return $page->build();
+    }
+}
