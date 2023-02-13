173637b0d08aa6119a9b9d849cfb6e2301e17585
nguyenanhung <info@nguyenanhung.com>
2023-02-14 00:18:12 +0700

    Update version 1.0.1

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -8,3 +8,21 @@ if (!function_exists('_simpleViewPagination_')) {
         return $page->build();
     }
 }
+if (!function_exists('_simpleViewMorePagination_')) {
+    function _simpleViewMorePagination_($data = array())
+    {
+        $page = new \nguyenanhung\Libraries\Pagination\Pagination\SimplePagination();
+        $page->setData($data);
+
+        return $page->buildViewMore();
+    }
+}
+if (!function_exists('_simpleViewSelectPagination_')) {
+    function _simpleViewSelectPagination_($data = array())
+    {
+        $page = new \nguyenanhung\Libraries\Pagination\Pagination\SimplePagination();
+        $page->setData($data);
+
+        return $page->buildSelectPage();
+    }
+}
