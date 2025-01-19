efaf40abbc6abcd17475ddb8ca5f0d1694cae7c4
nguyenanhung <hungna.dev@gmail.com>
2025-01-20 01:50:13 +0700

    Release v2.1.0

--- src/Pagination/BaseCore.php
+++ src/Pagination/BaseCore.php
@@ -12,7 +12,7 @@ namespace nguyenanhung\Libraries\Pagination\Pagination;
 
 class BaseCore
 {
-    const VERSION = '2.0.9.1';
+    const VERSION = '2.1.0';
 
     public function getVersion(): string
     {
